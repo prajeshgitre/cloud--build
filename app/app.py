3ee53628118d5852dba81512fbcdaca49a2a9d78
Prajesh <147793159+prajeshgitre@users.noreply.github.com>
2024-08-21 07:09:52 +0530

    Update app.py

--- app/app.py
+++ app/app.py
@@ -1,7 +1,5 @@
 import requests
-from flask import Flask
-from flask import jsonify
-
+from flask import Flask, jsonify
 
 instance_metadata_url = "http://metadata.google.internal/computeMetadata/v1/instance/"
 metadata_entries_to_grab = {"id": "instance-id", "name": "instance-name"}
@@ -9,25 +7,10 @@ metadata_flavor_header = {"Metadata-Flavor": "Google"}
 
 app = Flask(__name__)
 
-
 @app.route('/')
 def main():
-    return_resp = {"error": "Helloword1"}
+    return_resp = {"error": "Hello, World!"}
     return jsonify(return_resp)
 
-
-# @app.route('/')
-# def main():
-#     return_resp = {}
-#
-#     for metadata_entry in metadata_entries_to_grab:
-#         req = "{}{}".format(instance_metadata_url, metadata_entry)
-#         resp = requests.get(req, headers=metadata_flavor_header)
-#         return_resp[metadata_entries_to_grab[metadata_entry]] = resp.text
-#
-#     print(return_resp)
-#     return jsonify(return_resp)
-#
-
-# if __name__ == '__main__':
-#     app.run(host="0.0.0.0", port=80)
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=80)
