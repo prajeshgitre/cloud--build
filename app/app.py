92aa0a3fdd6a38326343170daee124b4175bf6fd
Prajesh <147793159+prajeshgitre@users.noreply.github.com>
2024-08-27 16:29:03 +0530

    Update app.py

--- app/app.py
+++ app/app.py
@@ -1,16 +1,10 @@
-import requests
 from flask import Flask, jsonify
-
-# Base URL for Google Cloud instance metadata
-instance_metadata_url = "http://metadata.google.internal/computeMetadata/v1/instance/"
-metadata_entries_to_grab = {"id": "instance-id", "name": "instance-name"}
-metadata_flavor_header = {"Metadata-Flavor": "Google"}
 
 app = Flask(__name__)
 
 @app.route('/')
 def main():
-    return_resp = {"error": "helloworld"}
+    return_resp = {"message": "helloworld"}
     return jsonify(return_resp)
 
 if __name__ == '__main__':
