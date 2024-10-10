958181e90b6558caee230d5bcaa1084d1b439091
Prajesh <147793159+prajeshgitre@users.noreply.github.com>
2024-10-10 10:48:47 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,8 +3,9 @@ var http = require('http')
 var port = 80
 
 var server = http.createServer(function (request, response) {
-  response.writeHead(200, {'Content-Type': 'text/plain'})
-  response.end('Hello World...bye..\n')
+  // Intentionally throwing an error to simulate a 500 Internal Server Error
+  response.writeHead(500, {'Content-Type': 'text/plain'})
+  response.end('Internal Server Error\n')
 })
 
 server.listen(port)
